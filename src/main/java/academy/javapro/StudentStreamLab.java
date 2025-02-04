2da034051cbd338590a563613566d47bcaf9d795
Ryan <ryan.sarjoo21@my.stjohns.edu>
2025-02-03 22:34:42 -0500

    finished homework

--- src/main/java/academy/javapro/StudentStreamLab.java
+++ src/main/java/academy/javapro/StudentStreamLab.java
@@ -14,12 +14,44 @@ public class StudentStreamLab {
                 new Student("Eve", 3.9, "Junior")
         );
 
-        // TODO - Filtering: Students with GPA > 3.0
+        // Filtering: Students with GPA > 3.0
+        List<Student> highGpaStudents = students.stream()
+                .filter(s -> s.getGpa() > 3.0)
+                .collect(Collectors.toList());
 
-        // TODO - Mapping: Extract names of students with GPA > 3.0
+        System.out.println("Students with GPA > 3.0:");
+        for (Student s : highGpaStudents) {
+            System.out.println(
+                s.getName() + " (" + s.getCollegeYear() + ") - GPA: " + s.getGpa()
+            );
+        }
+        System.out.println();
 
-        // TODO - Reducing: Calculate the average GPA of all students
+        // Mapping: Extract names of students with GPA > 3.0
+        System.out.println("High GPA student names:");
+        highGpaStudents.stream()
+                .map(Student::getName)
+                .forEach(System.out::println);
+        System.out.println();
 
-        // TODO Collecting: Collect all "Junior" students into a list
+        // Reducing: Calculate the average GPA of all students
+        double averageGpa = students.stream()
+                .mapToDouble(Student::getGpa)
+                .average()
+                .orElse(0.0);
+
+        System.out.printf("Average GPA: %.2f\n\n", averageGpa);
+
+        // Collecting: Collect all "Junior" students into a list
+        List<Student> juniors = students.stream()
+                .filter(s -> "Junior".equals(s.getCollegeYear()))
+                .collect(Collectors.toList());
+
+        System.out.println("Juniors:");
+        for (Student j : juniors) {
+            System.out.println(
+                j.getName() + " (" + j.getCollegeYear() + ") - GPA: " + j.getGpa()
+            );
+        }
     }
 }
